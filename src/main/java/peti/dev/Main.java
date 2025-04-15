35769d102353bc6488eb47f9662a93ed0047c06c
Péter Kelemen <peter.kelemen456@gmail.com>
2025-04-15 18:08:25 +0200

    Modifications for testing purposes

--- src/main/java/peti/dev/Main.java
+++ src/main/java/peti/dev/Main.java
@@ -13,36 +13,42 @@ public class Main {
         long cycleTime = 1000;
 
         Entity entity = new Entity("sad");
-        System.out.println(entity);
+//        System.out.println(entity);
 
         Player player = new Player("player", 10, 1);
         System.out.println(player);
 
         Enemy enemy = new Enemy("enemy", 4, 1);
-        System.out.println(enemy);
+//        System.out.println(enemy);
 
 //        player.attack(enemy);
-        CombatController combatController = new CombatController(player, new Enemy[]{enemy});
+//        CombatController combatController = new CombatController(player, new Enemy[]{enemy});
 
-        TurnController turnController = new TurnController();
+//        TurnController turnController = new TurnController(player);
+
+        Enemy lvl4 = new Enemy("SADASDD", 4);
+        System.out.println(lvl4);
+
+        player.levelUp();
+        System.out.println(player);
 
 
-        while (true) {
-            long startTime = System.nanoTime();
-//            System.out.println("Start: " + startTime);
-
-//            combatController.doTurn();
-            turnController.doTurn();
-
-            long endTime = System.nanoTime();
-//            System.out.println("End: " + endTime);
-
-            long timeTaken = (endTime - startTime) / 1_000_000;
-            System.out.println("Cycle took " + timeTaken + "ms");
-
-            if (timeTaken < cycleTime) {
-                Thread.sleep(cycleTime - timeTaken);
-            }
-        }
+//        while (true) {
+//            long startTime = System.nanoTime();
+////            System.out.println("Start: " + startTime);
+//
+////            combatController.doTurn();
+//            turnController.doTurn();
+//
+//            long endTime = System.nanoTime();
+////            System.out.println("End: " + endTime);
+//
+//            long timeTaken = (endTime - startTime) / 1_000_000;
+//            System.out.println("Cycle took " + timeTaken + "ms");
+//
+//            if (timeTaken < cycleTime) {
+//                Thread.sleep(cycleTime - timeTaken);
+//            }
+//        }
     }
 }
